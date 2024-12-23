6b895eefa55347eaf74483b3ebac6c059e0b5358
1parkerj1 <1parkerj1@gmail.com>
2024-12-23 12:27:18 +0000

    ADDED JUNIT TEST FOR GAME

---
+++ src/game/GameTest.java
@@ -0,0 +1,33 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void startGame() {
+        
+    }
+
+    @Test
+    void playRound() {
+    }
+
+    @Test
+    void getDeckCards() {
+    }
+
+    @Test
+    void getRemainingCards() {
+    }
+
+    @Test
+    void getScore() {
+    }
+
+    @Test
+    void getReplayQueue() {
+    }
+}
